a8885b0b4e86abe17e6158f80741cb38b26df7ef
Myrinchen <myrine.schmid@hak-reutte.ac.at>
2024-03-12 13:04:01 +0100

    änderung das Charaktäre selber angelegt werden können

--- src/arena/App.java
+++ src/arena/App.java
@@ -3,11 +3,58 @@ package arena;
 import java.util.Scanner;
 
 public class App {
-	static Dragon d1 = new Dragon("Kokusnuss");
-	static Dwarf d2 = new Dwarf("Zwergii");
-	public static Arena a = new Arena(d1,d2);
+	public static Charakter d1 = null;
+	public static Charakter d2 = null;
+	static String name ="";
+	static int wahl = 0;
+	public static int eingabeCharakter(Scanner sc) {
+		System.out.println("Bitte wähle zwischen: \n\t" + "|1| Drache \n\t" + "|2| Zwerg");
+		
+		wahl = -1;
+		do {
+			wahl = sc.nextInt();
+		}while (!(wahl >= 1 && wahl <=2));
+		return wahl;
+	}
 	
-	public static int eingabe(Scanner sc) {
+	public static String eingabeNameOfCharakter(Scanner sc) {
+		System.out.println("Bitte wähle einen Namen : ");
+		name = "";
+		do {
+		name = sc.nextLine();
+		}while (name.equals(""));
+		return name;
+	}
+	
+	public static Charakter createCharakter(int wahl, String name) {
+		//for(int i = 0; i<3; i++) {
+		//if(i == 1) {
+			if(wahl == 1) {
+				 //d1 = new Dragon(name);
+				return new Dragon(name);
+			}else if (wahl == 2) {
+				//d1 = new Dwarf(name);
+				return new Dwarf(name);
+				
+			}else {
+				System.out.println("ihre Eingabe war Fehlerhaft, Versuchen sie es bitte erneut :");
+			}
+			return null;
+//		}else if(i == 2) {
+//			if(wahl == 1) {
+//				d2 = new Dragon(name);
+//			}else if (wahl == 2) {
+//				d2 = new Dwarf(name);
+//			}else {
+//				System.out.println("ihre Eingabe war Fehlerhaft, Versuchen sie es bitte erneut :");
+//			}
+//		}
+//	}
+	}
+	
+	//public static 
+	
+	public static int eingabe(Scanner sc, Arena a) {	
 		System.out.println( a.amZug.getName() + " ist am Zug \n");
 		System.out.println("bitte wähle deinen Zug");
 		System.out.println("|1| Angreifen \n"+"|2| Spezialfähigkeit Aktivieren \n" + "|3| Spezialfähigkeit Deaktivieren \n");
@@ -20,11 +67,22 @@ public class App {
 	}
 
 	public static void main(String[] args) {
+		
 		int eingabe;
 		Scanner sc = new Scanner(System.in);
+		do {
+			wahl = eingabeCharakter(sc);
+			name = eingabeNameOfCharakter(sc);
+			d1 = createCharakter(wahl,name);
+			wahl = eingabeCharakter(sc);
+			name = eingabeNameOfCharakter(sc);
+			d2 = createCharakter(wahl,name);
+		} while (d1.equals(null) || d2.equals(null));
+		
+		Arena a = new Arena(d1,d2);
 		a.beginner();
 		do {
-			eingabe = eingabe(sc);
+			eingabe = eingabe(sc, a);
 			a.fight(eingabe);
 			a.checkForWinner();
 			
