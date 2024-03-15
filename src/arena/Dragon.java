a3a03ef1b1538b51341fc4ad5ecc6eb2d2683f9d
DavidHundertpfund <david.hundertpfund@hak-reutte.ac.at>
2024-03-15 10:29:23 +0100

    added Java-Doc Comments for Dragon class

--- src/arena/Dragon.java
+++ src/arena/Dragon.java
@@ -1,40 +1,51 @@
-package arena;
-import java.util.concurrent.ThreadLocalRandom;
-
-public class Dragon extends Charakter{
-
-	public Dragon(String name) {
-		super(name);
+	package arena;
+	import java.util.concurrent.ThreadLocalRandom;
+	
+	public class Dragon extends Charakter{
+	
+		public Dragon(String name) {
+			super(name);
+				}
+		/**
+	     * Methode generiert  Schadenspunkte.
+	     * Wenn die Spezialfähigkeit aktiv ist, werden die Schadenspunkte reduziert.
+	     *
+	     * @return generierten Schadenspunkte.
+	     */
+		public int randompoints() {
+			int randomPoints = 0;
+			if(isSpecialAbilityActive() == false) {
+				 randomPoints = ThreadLocalRandom.current().nextInt(20, 25);
 			}
-
-	public int randompoints() {
-		int randomPoints = 0;
-		if(isSpecialAbilityActive() == false) {
-			 randomPoints = ThreadLocalRandom.current().nextInt(20, 25);
-		}
-		else if(isSpecialAbilityActive() == false) {
-			 randomPoints = ThreadLocalRandom.current().nextInt(5, 10);
-		}
-		
-		return randomPoints;
+			else if(isSpecialAbilityActive() == false) {
+				 randomPoints = ThreadLocalRandom.current().nextInt(5, 10);
+			}
+			
+			return randomPoints;
+				
+			}
+	
+	
+		 /**
+	     * Methode aktiviert die Spezialfähigkeit des Drachens.
+	     * Erhöht die Lebenspunkte des Drachens um 10.
+	     */
+		public void activateSpecialAbillity() {
+			this.setSpecialAbilityActive(true);
+			this.setLifepoints(getLifepoints()+10);
 			
 		}
-
-
 	
-	public void activateSpecialAbillity() {
-		this.setSpecialAbilityActive(true);
-		this.setLifepoints(getLifepoints()+10);
+		/**
+	     * Methode deaktiviert die Spezialfähigkeit des Drachens.
+	     *  Verringert die Lebenspunkte des Drachens um 10.
+	     */
+		public void deactivateSpecialAbillity() {
+			this.setSpecialAbilityActive(false);
+			this.setLifepoints(getLifepoints()-10);
+			
+		}
 		
-	}
-
-	
-	public void deactivateSpecialAbillity() {
-		this.setSpecialAbilityActive(false);
-		this.setLifepoints(getLifepoints()-10);
+			
+		}
 		
-	}
-	
-		
-	}
-	
